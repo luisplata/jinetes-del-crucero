870a7f5695cf2164fcab6e3ab2376bbe24ba351b
Luis Plata <www.luisplata@gmail.com>
2020-05-18 20:38:20 -0500

    Se termina la configuracion del controlador de sonidos del juego

--- Assets/ControladorDeVolumenDeSonidos.cs
+++ Assets/ControladorDeVolumenDeSonidos.cs
@@ -5,24 +5,27 @@ using UnityEngine.UI;
 
 public class ControladorDeVolumenDeSonidos : MonoBehaviour {
     //Lo vamos a hacer con una lista de AudioSource para que los cambios se realicen de manera paralela con todos los cuestionados aqui
-    public AudioSource pista1, pista2, pista3, pista4;
+    public AudioSource pista1, pista2, pista3, pista4, pista5;
     public Slider slider;
+    //[SerializeField]
+    private float pista1Max, pista2Max, pista3Max, pista4Max, pista5Max;
 
-    private float pista1Max, pista2Max, pista3Max, pista4Max;
-    private float playerPref1, playerPref2, playerPref3, playerPref4;
-
+    float volumenFlotante;
     private void Start () {
-        //vamos a consultar los valores guardados en playerpref
-        if (PlayerPrefs.HasKey ("volumenGeneral")) {
-            slider.value = PlayerPrefs.GetFloat ("volumenGeneral");
-        } else {
-            slider.value = 1;
-        }
-        CambioDeVolumenMejorado (slider.value);
         pista1Max = pista1.volume;
         pista2Max = pista2.volume;
         pista3Max = pista3.volume;
         pista4Max = pista4.volume;
+        pista5Max = pista5.volume;
+        //vamos a consultar los valores guardados en playerpref
+        if (PlayerPrefs.HasKey ("volumenGeneral")) {
+            float numeroFlotante = (float) PlayerPrefs.GetInt ("volumenGeneral");
+            volumenFlotante = numeroFlotante / 100;
+        } else {
+            volumenFlotante = 1;
+        }
+        CambioDeVolumenMejorado (volumenFlotante);
+        slider.value = volumenFlotante;
     }
     public void CambioDeVolumen () {
         CambioDeVolumenMejorado (slider.value);
@@ -34,7 +37,8 @@ public class ControladorDeVolumenDeSonidos : MonoBehaviour {
         pista2.volume = volumen * pista2Max;
         pista3.volume = volumen * pista3Max;
         pista4.volume = volumen * pista4Max;
+        pista5.volume = volumen * pista5Max;
         //guardamos en discoduro
-        PlayerPrefs.SetFloat ("volumenGeneral", volumen);
+        PlayerPrefs.SetInt ("volumenGeneral", (int) (volumen * 100));
     }
 }
